b84e60410ac9088efe91edac416b733d5749d1d5
arebelo <anagmrebelo@hotmail.com>
2023-08-28 00:09:33 +0200

    Intro without link to projects

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,5 +1,41 @@
+import { VscGithubAlt } from "react-icons/vsc";
+import { BsLinkedin } from "react-icons/bs";
+
 function Intro(): JSX.Element {
-    return <></>;
+    return (
+        <div>
+            <div className="intro-hello">
+                <div>
+                    <h3 className="small-heading">Hi, my name is</h3>
+                    <h1 className="large-heading">Ana Rebêlo.</h1>
+                    <h2 className="large-heading sec-text-color">
+                        I am a Software Developer
+                    </h2>
+                    <button className="btn-m mg-40-td ">
+                        Check out my projects
+                    </button>
+                </div>
+            </div>
+            <div className="intro-icons">
+                <nav className="nav-icons">
+                    <a
+                        href="https://github.com/anagmrebelo"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <VscGithubAlt size={32} color="white" />
+                    </a>
+                    <a
+                        href="https://www.linkedin.com/in/ana-mota-rebelo/"
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <BsLinkedin size={32} color="white" />
+                    </a>
+                </nav>
+            </div>
+        </div>
+    );
 }
 
 export { Intro };
